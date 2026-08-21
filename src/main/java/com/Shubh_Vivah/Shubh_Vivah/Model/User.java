4f8f13fcad5ca60765d5655fdeabcbe99793ff86
RAM VASHISHTHA <92358898+ram1071999@users.noreply.github.com>
2026-08-21 15:18:59 +0530

    Update User.java

--- src/main/java/com/Shubh_Vivah/Shubh_Vivah/Model/User.java
+++ src/main/java/com/Shubh_Vivah/Shubh_Vivah/Model/User.java
@@ -28,7 +28,7 @@ public class User {
     private String gender;
 
     @NotNull(message = "Date of birth is required")
-    private Date dob;
+    private LocalDate dob;
 
     private String religion;
 
@@ -90,11 +90,11 @@ public class User {
         this.gender = gender;
     }
 
-    public Date getDob() {
+    public LocalDate getDob() {
         return dob;
     }
 
-    public void setDob(Date dob) {
+    public void setDob(LocalDate dob) {
         this.dob = dob;
     }
 
