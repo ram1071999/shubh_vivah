baf62b4f99ac6d179e5cfa18bfbb053dcb99b5d3
RAM VASHISHTHA <92358898+ram1071999@users.noreply.github.com>
2026-08-26 09:24:48 +0530

    Update LoginController.java

--- src/main/java/com/Shubh_Vivah/Shubh_Vivah/Controller/LoginController.java
+++ src/main/java/com/Shubh_Vivah/Shubh_Vivah/Controller/LoginController.java
@@ -1,17 +1,23 @@
 package com.Shubh_Vivah.Shubh_Vivah.Controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.Shubh_Vivah.Shubh_Vivah.Security.CustomUserDetails;
 
 @Controller
 public class LoginController {
-	
-	@GetMapping("/login")
-	public String showloginpage()
-	{
-		
-		return "login";
-	}
 
+    @GetMapping("/login")
+    public String showloginpage() {
+        return "login";
+    }
+
+    @GetMapping("/dashboard")
+    public String showDashboard(@AuthenticationPrincipal CustomUserDetails principal, Model model) {
+        model.addAttribute("user", principal.getUser());
+        return "dashboard";
+    }
 }
-
