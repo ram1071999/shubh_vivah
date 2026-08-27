acc0065c107120e4bf2f14e5df5e913ccb38d9c9
RAM VASHISHTHA <92358898+ram1071999@users.noreply.github.com>
2026-08-27 14:31:25 +0530

    Update CustomUserDetailsService.java

--- src/main/java/com/Shubh_Vivah/Shubh_Vivah/Config/CustomUserDetailsService.java
+++ src/main/java/com/Shubh_Vivah/Shubh_Vivah/Config/CustomUserDetailsService.java
@@ -1,4 +1,4 @@
-package com.Shubh_Vivah.Shubh_Vivah.Security;
+package com.Shubh_Vivah.Shubh_Vivah.Config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
